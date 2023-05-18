47deaec043c0653ec8bb27c87092104dda043991
M.J <janelidze.marita@kiu.edu.ge>
2023-05-18 17:57:48 +0400

    Update

--- Problem_1.3/CubedSphere.py
+++ Problem_1.3/CubedSphere.py
@@ -1,48 +1,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-def compute_derivatives(f, dx):
-    # Compute derivatives using finite difference approximation
-    dfdx = (f[2, 1, :] - f[0, 1, :]) / (2 * dx)
-    dfdy = (f[1, 2, :] - f[1, 0, :]) / (2 * dx)
-    dfdz = (f[1, 1, 1] - f[1, 1, 0]) / (dx)  # Assumes 2-layer grid in the z-direction
-    return dfdx, dfdy, dfdz
+def generate_cubed_sphere_mesh():
+    # Define the number of subdivisions
+    subdivisions = 10
 
-def test_convergence():
-    # Define a test function on a sphere
-    def f(x, y, z):
-        return np.sin(x) * np.cos(y) * np.exp(z)
+    # Generate the vertices of the cubed sphere
+    vertices = []
+    for x in range(-subdivisions, subdivisions + 1):
+        for y in range(-subdivisions, subdivisions + 1):
+            for z in range(-subdivisions, subdivisions + 1):
+                vertices.append((x, y, z))
+    vertices = np.array(vertices, dtype=np.float64)  # Store vertices as float
 
-    # Parameters
-    num_samples = 5
-    grid_spacings = np.logspace(-2, -6, num_samples)  # Varying grid spacings
-    errors = np.zeros(num_samples)
+    # Normalize the vertices
+    norm = np.linalg.norm(vertices, axis=1)
+    vertices /= norm[:, None]
 
-    for i, dx in enumerate(grid_spacings):
-        # Compute derivatives using finite differences
-        x = np.linspace(-np.pi/2, np.pi/2, 3)
-        y = np.linspace(-np.pi/2, np.pi/2, 3)
-        z = np.linspace(-np.pi/2, np.pi/2, 2)
-        X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
-        F = f(X, Y, Z)
-        derivatives = compute_derivatives(F, dx)
+    # Create the figure and 3D axis
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-        # Compute exact derivatives
-        exact_derivatives = (np.cos(X) * np.cos(Y) * np.exp(Z),
-                             -np.sin(X) * np.sin(Y) * np.exp(Z),
-                             np.sin(X) * np.cos(Y) * np.exp(Z))
+    # Plot the cubed sphere mesh
+    ax.scatter(vertices[:, 0], vertices[:, 1], vertices[:, 2], s=1)
 
-        # Compute error
-        errors[i] = np.sqrt(np.mean((np.array(derivatives) - np.array(exact_derivatives))**2))
+    # Set axis limits and labels
+    ax.set_xlim([-1.1, 1.1])
+    ax.set_ylim([-1.1, 1.1])
+    ax.set_zlim([-1.1, 1.1])
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
-    return grid_spacings, errors
+    # Show the plot
+    plt.show()
 
-# Perform convergence test
-grid_spacings, errors = test_convergence()
-
-# Plot results
-plt.loglog(grid_spacings, errors, '-o')
-plt.xlabel('Grid Spacing (dx)')
-plt.ylabel('Error')
-plt.title('Convergence of Derivative Approximation')
-plt.show()
+# Generate and visualize the cubed sphere mesh
+generate_cubed_sphere_mesh()
---
+++ Problem_1.3/README.md
@@ -0,0 +1,20 @@
+## [Cubed Sphere](CubedSphere.py)
+Given a point (x, y, z) on the unit cube where -1 ≤ x, y, z ≤ 1:
+
+Determine the face of the cube the point lies on based on its maximum absolute coordinate value. The six faces of the cube are: +X, -X, +Y, -Y, +Z, -Z.
+
+Map the coordinates onto the unit sphere using the following formulas for each face:
+
+    +X face: (1, -y, -z)
+    -X face: (-1, y, z)
+    +Y face: (x, 1, -z)
+    -Y face: (x, -1, z)
+    +Z face: (x, -y, 1)
+    -Z face: (-x, -y, -1)
+Normalize the resulting coordinates (x', y', z') by dividing each component by the Euclidean length of the vector (x', y', z').
+
+The resulting (x', y', z') coordinates will lie on the unit sphere and form a cubed sphere mesh.
+
+Note: The above representation assumes a unit cube, but the scaling and positioning of the cube can be adjusted as needed.
+
+![screenshot](img.png)
